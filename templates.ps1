9873a74e073f9dd160b2e082df71418afc28aa8c
Jyquas <53891203+Jyquas@users.noreply.github.com>
2023-11-10 10:46:44 +1100

    Update templates.ps1

--- templates.ps1
+++ templates.ps1
@@ -5,7 +5,26 @@ Connect-PnPOnline -Url $siteUrl -Credentials $cred
 
 # Retrieve files from the Master Page Gallery
 $files = Get-PnPListItem -List "Master Page Gallery" | Where-Object { $_["ContentType"] -like "*intranet*" }
+# Connect to SharePoint Online
+$siteUrl = "your_site_url"
+$cred = Get-Credential
+Connect-PnPOnline -Url $siteUrl -Credentials $cred
 
+# Get the ID of the 'intranet' content type
+$contentTypes = Get-PnPContentType
+$intranetContentTypeId = $contentTypes | Where-Object { $_.Name -like "*intranet*" } | Select-Object -ExpandProperty Id
+
+# Check if the 'intranet' content type exists
+if ($intranetContentTypeId) {
+    # Construct the CAML query
+    $camlQuery = New-Object Microsoft.SharePoint.Client.CamlQuery
+    $camlQuery.ViewXml = "<View><Query><Where><Eq><FieldRef Name='ContentTypeId'/><Value Type='ContentTypeId'>$intranetContentTypeId</Value></Eq></Where></Query></View>"
+
+    # Retrieve files from the Master Page Gallery
+    $files = Get-PnPListItem -List "Master Page Gallery" -Query $camlQuery -Fields "FileRef", "FileLeafRef"
+
+    # Process each file
+    
 # Process each file
 foreach ($file in $files) {
     $fileUrl = $file["FileRef"]
