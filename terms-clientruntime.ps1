7be77d744ca747970414eeac232c956c62182440
Jyquas <53891203+Jyquas@users.noreply.github.com>
2023-11-14 12:03:40 +1100

    Create terms-clientruntime.ps1

---
+++ terms-clientruntime.ps1
@@ -0,0 +1,69 @@
+# Load SharePoint CSOM assemblies
+Add-Type -Path "path\to\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "path\to\Microsoft.SharePoint.Client.Runtime.dll"
+Add-Type -Path "path\to\Microsoft.SharePoint.Client.Taxonomy.dll"
+
+# SharePoint site details
+$siteUrl = "https://yourtenant.sharepoint.com/sites/yoursite"
+$username = "your-username"
+$password = ConvertTo-SecureString "your-password" -AsPlainText -Force
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
+$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $password)
+
+# Access taxonomy session, term store, term group, and term set
+$taxonomySession = [Microsoft.SharePoint.Client.Taxonomy.TaxonomySession]::GetTaxonomySession($ctx)
+$termStore = $taxonomySession.GetDefaultSiteCollectionTermStore()
+$termGroup = $termStore.GetGroup("your-term-group-id")
+$termSet = $termGroup.GetTermSet("your-term-set-id")
+$ctx.Load($termSet)
+$ctx.ExecuteQuery()
+
+# Function to process terms
+function Process-Terms {
+    param (
+        [Parameter(Mandatory = $true)]
+        $Terms
+    )
+
+    $result = @()
+
+    foreach ($term in $Terms) {
+        $ctx.Load($term)
+        $ctx.ExecuteQuery()
+
+        # Assuming target URL is stored in a local custom property
+        $targetUrl = $term.LocalCustomProperties["TargetUrl"]
+
+        # Retrieve the navigation term to get the friendly URL
+        $navTerm = [Microsoft.SharePoint.Client.Taxonomy.NavigationTerm]::GetNavigationTerm($term)
+        $ctx.Load($navTerm)
+        $ctx.ExecuteQuery()
+
+        $termData = @{
+            TermId = $term.Id
+            TargetUrl = $targetUrl
+            FriendlyUrl = $navTerm.GetResolvedDisplayUrl($null)
+        }
+        $result += $termData
+
+        if ($term.Terms) {
+            $childResults = Process-Terms -Terms $term.Terms
+            $result += $childResults
+        }
+    }
+
+    return $result
+}
+
+# Retrieve and process terms
+$terms = $termSet.GetAllTerms()
+$ctx.Load($terms)
+$ctx.ExecuteQuery()
+
+$termMapping = Process-Terms -Terms $terms
+
+# Export to JSON
+$termMapping | ConvertTo-Json -Depth 100 | Out-File "term-mapping.json"
+
+# Output completion message
+Write-Host "Term mapping exported to term-mapping.json"
