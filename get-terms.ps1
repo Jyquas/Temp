c31e007945c17eea39a7391ffd0f14339cbf6505
Jyquas <53891203+Jyquas@users.noreply.github.com>
2023-11-14 11:26:09 +1100

    Update get-terms.ps1

--- get-terms.ps1
+++ get-terms.ps1
@@ -1,25 +1,38 @@
-# Function to recursively process terms and build a hierarchical structure
 function Process-Terms {
     param (
         [Parameter(Mandatory = $true)]
-        $Terms
+        $Terms,
+        $TermStore,
+        $TermSetId,
+        $NewUrlBase
     )
 
     $result = @()
 
     foreach ($term in $Terms) {
-        $termData = @{
-            Term = $term.Name
-            Id = $term.Id
-            LocalCustomProperties = $term.LocalCustomProperties
+        # Retrieve the associated navigation term
+        $navTerm = $TermStore.GetTerm($term.Id)
+        $TermStore.Context.Load($navTerm)
+        $TermStore.Context.ExecuteQuery()
+
+        if ($navTerm.TargetUrl -ne $null) {
+            # Construct the new URL based on the term data
+            $newUrl = $NewUrlBase + "[new page path based on term data].aspx"
+            
+            # Build term data
+            $termData = @{
+                TermSetId = $TermSetId
+                TermId = $term.Id.ToString()
+                NewUrl = $newUrl
+            }
+            $result += $termData
         }
 
-        # If the term has child terms, process them recursively
+        # Process child terms if any
         if ($term.Terms -and $term.Terms.Count -gt 0) {
-            $termData["ChildTerms"] = Process-Terms -Terms $term.Terms
+            $childResults = Process-Terms -Terms $term.Terms -TermStore $TermStore -TermSetId $TermSetId -NewUrlBase $NewUrlBase
+            $result += $childResults
         }
-
-        $result += $termData
     }
 
     return $result
@@ -29,23 +42,26 @@ function Process-Terms {
 $siteUrl = "https://yourtenant.sharepoint.com/sites/yoursite"
 $termGroupId = "your-term-group-id"
 $termSetName = "your-term-set-name"
-$outputFile = "terms.json"
+$termSetId = "your-term-set-id" # Add the correct Term Set ID
+$newUrlBase = "https://yourtenant.sharepoint.com/sites/newsite/sitepages/"
+$outputFile = "term-mapping.json"
 
 # Connect to SPO
 Connect-PnPOnline -Url $siteUrl -Interactive
 
-# Retrieve the term set from the specified term group
+# Retrieve the term set and term store
 $termGroup = Get-PnPTermGroup -Identity $termGroupId
 $termSet = Get-PnPTermSet -Identity $termSetName -TermGroup $termGroup
+$termStore = $termSet.TermStore
 
-# Retrieve terms and include child terms
+# Retrieve terms including child terms
 $terms = Get-PnPTerm -TermSet $termSet -TermGroup $termGroup -IncludeChildTerms
 
-# Process terms and build hierarchy
-$hierarchicalTerms = Process-Terms -Terms $terms
+# Process terms to build mapping
+$termMapping = Process-Terms -Terms $terms -TermStore $termStore -TermSetId $termSetId -NewUrlBase $newUrlBase
 
 # Write to JSON
-$hierarchicalTerms | ConvertTo-Json -Depth 10 | Out-File $outputFile
+$termMapping | ConvertTo-Json -Depth 100 | Out-File $outputFile
 
 # Output completion message
-Write-Host "Term data exported to $outputFile"
+Write-Host "Term mapping exported to $outputFile"
