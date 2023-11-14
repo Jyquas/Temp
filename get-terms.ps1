f57bd6face7e43e3250068f22b2d2a00206df4a3
Jyquas <53891203+Jyquas@users.noreply.github.com>
2023-11-14 14:05:50 +1100

    Update get-terms.ps1

--- get-terms.ps1
+++ get-terms.ps1
@@ -108,6 +108,30 @@ function ProcessFullPathTerms {
     return $result
 }
 
+function Find-TermById {
+    param (
+        [Parameter(Mandatory = $true)]
+        $Terms,
+        [Parameter(Mandatory = $true)]
+        $TermId
+    )
+
+    foreach ($term in $Terms) {
+        if ($term.Id -eq $TermId) {
+            return $term
+        }
+
+        if ($term.ChildTerms) {
+            $foundTerm = Find-TermById -Terms $term.ChildTerms -TermId $TermId
+            if ($foundTerm) {
+                return $foundTerm
+            }
+        }
+    }
+
+    return $null
+}
+
 
 
 # Variables
