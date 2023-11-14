58ec2c811d9d59a1b515092b72a2cf7a63eac347
Jyquas <53891203+Jyquas@users.noreply.github.com>
2023-11-14 14:27:01 +1100

    Update findterm.ps1

--- findterm.ps1
+++ findterm.ps1
@@ -21,3 +21,40 @@ function Find-TermById {
 
     return $null
 }
+
+# Variables
+$siteUrl = "https://yourtenant.sharepoint.com/sites/yoursite"
+$classicPageUrl = "path/to/classic/page"
+$modernPageName = "ModernPageName.aspx"
+$staticWebId = "your-static-webid" # Replace with actual WebID
+$staticTermSetId = "your-static-termsetid" # Replace with actual TermSetID
+
+# Connect to the SharePoint site
+Connect-PnPOnline -Url $siteUrl -Interactive
+
+# Retrieve content from the classic page (assuming it's stored in a field like 'PublishingPageContent')
+$classicPageContent = Get-PnPListItem -List "Pages" -Query "<View><Query><Where><Eq><FieldRef Name='FileRef'/><Value Type='Url'>$classicPageUrl</Value></Eq></Where></Query></View>"
+
+# Process the content to replace URLs
+$pattern = "fixupredirect\.aspx\?WebID=$staticWebId&termSetID=$staticTermSetId&termId=([\w-]+)"
+foreach ($item in $classicPageContent) {
+    $content = $item["PublishingPageContent"]
+
+    if ($content -match $pattern) {
+        $termId = $matches[1]
+        $foundTerm = Find-TermById -Terms $termMappings -TermId $termId
+
+        if ($foundTerm) {
+            $newUrl = $foundTerm.FriendlyUrlSegment
+            $content = $content -replace $pattern, $newUrl
+        }
+    }
+
+    # Create a new client-side page and add the processed content
+    $page = Set-PnPClientSidePage -Name $modernPageName -LayoutType Article -CommentsEnabled:$false
+    Add-PnPClientSideText -Page $page -Section 1 -Column 1 -Text $content
+}
+
+# Output completion message
+Write-Host "Content migrated to modern page: $modernPageName"
+
